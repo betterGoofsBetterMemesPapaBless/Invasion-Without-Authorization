b9ee8723dc099ea152601b1a7ee0815474fe3abe
Max Blackwell <maxmac001@gmail.com>
2017-08-10 10:47:38 -0700

    archiving

---
+++ Archer Invasion/Archer Invasion/LaunchBoard.swift
@@ -0,0 +1,36 @@
+//
+//  LaunchBoard.swift
+//  Archer Invasion
+//
+//  Created by Max Blackwell on 8/10/17.
+//  Copyright © 2017 Max Blackwell. All rights reserved.
+//
+
+import UIKit
+
+class LaunchBoard: UIViewController {
+
+    @IBOutlet weak var actionImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
